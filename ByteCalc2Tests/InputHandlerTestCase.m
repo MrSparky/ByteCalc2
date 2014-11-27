a0a1f4047d25259fa88687c440e21ce1ba84676f
Sean Parker <skparker@gmail.com>
2014-11-27 15:27:50 -0800

    Add tests for Input switching & base-conversion

--- ByteCalc2Tests/InputHandlerTestCase.m
+++ ByteCalc2Tests/InputHandlerTestCase.m
@@ -27,6 +27,42 @@
     [super tearDown];
 }
 
+-(void)testInputConvert {
+    InputHandler * ih = [[InputHandler alloc] init];
+    [ih setInputMode:InputModeDecimal];
+    // Ensure changing input base does not change result, but changes text
+    [ih setInputValue:10];
+    XCTAssertEqual([ih integerValue], 10);
+    XCTAssertEqualObjects([ih textValue], @"10");
+    [ih setInputMode:InputModeHexidecimal];
+    XCTAssertEqual([ih integerValue], 10);
+    XCTAssertEqualObjects([ih textValue], @"A");
+    [ih setInputMode:InputModeOctal];
+    XCTAssertEqual([ih integerValue], 10);
+    XCTAssertEqualObjects([ih textValue], @"12");
+
+    // Ensure input in various bases results in correct output
+    [ih resetInput];
+    [ih setInputMode:InputModeHexidecimal];
+    [ih appendInputWithChar:'F'];
+    XCTAssertEqual([ih integerValue], 15);
+    [ih setInputMode:InputModeOctal];
+    XCTAssertEqualObjects([ih textValue], @"17");
+    [ih appendInputWithChar:'5'];
+    XCTAssertEqual([ih integerValue], 125);
+    [ih setInputMode:InputModeHexidecimal];
+    XCTAssertEqualObjects([ih textValue], @"7D");
+    [ih appendInputWithChar:'A'];
+    XCTAssertEqual([ih integerValue], 2010);
+    [ih setInputMode:InputModeDecimal];
+    XCTAssertEqualObjects([ih textValue], @"2010");
+    [ih appendInputWithChar:'2'];
+    XCTAssertEqual([ih integerValue], 20102);
+    [ih setInputMode:InputModeHexidecimal];
+    XCTAssertEqualObjects([ih textValue], @"4E86");
+    
+}
+
 - (void)testBaseConvert {
     // Test Binary Base Conversion
     XCTAssertEqual([InputHandler integerFromString:@"1" withBase:2], 1);
@@ -67,6 +103,8 @@
     XCTAssertEqual([InputHandler integerFromString:@"CDEF" withBase:16], 0xCDEF);
     XCTAssertEqual([InputHandler integerFromString:@"0FF0" withBase:16], 0x0FF0);
     XCTAssertEqual([InputHandler integerFromString:@"AA55" withBase:16], 0xAA55);
+    // Test Hexidecimal Base w/ leading 0x
+    //XCTAssertEqual([InputHandler integerFromString:@"0xAA" withBase:16], 0xAA);
     
 }
 
